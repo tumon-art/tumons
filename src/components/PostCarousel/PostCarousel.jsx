1dba754c673d1cd714a0bb7e0668ba224783ce64
Tumon <tumonkhn@gmail.com>
2022-07-14 14:57:00 +0600

    .

--- src/components/PostCarousel/PostCarousel.jsx
+++ src/components/PostCarousel/PostCarousel.jsx
@@ -16,20 +16,13 @@ const PostCarousel = ({ posts }) => {
 
   return (
     <div className={styles.main}>
-      <svg className={styles.svgL}
-        onClick={leftArrow}
-        stroke="currentColor" strokeWidth={2}
-        viewBox="0 0 24 24" fill="none">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
+      
+      <span className={styles.svgL}
+      onClick={leftArrow}> ❰</span>
 
-      </svg>
 
-      <svg className={styles.svgR}
-        onClick={rightArrow}
-        stroke="currentColor" strokeWidth={2}
-        viewBox="0 0 24 24" fill="none">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>
+      <span className={styles.svgR}
+      onClick={rightArrow}> ❱ </span>
       
       <div id='auto' className={styles.carouselHold}>
       {posts.map((post) => (
