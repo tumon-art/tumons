a2cbcd91c0f0c0ef9cb449d942df6792832c78e1
Tumon <tumonkhn@gmail.com>
2022-07-21 02:32:29 +0600

    init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
         <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-        <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans&display=swap" rel="stylesheet" />
         
         </Head>
 
