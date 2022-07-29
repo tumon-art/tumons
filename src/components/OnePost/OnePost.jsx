5c790021586274319f7e608274be03eaa41d1e32
tumon <tumonkhn@gmail.com>
2022-07-30 05:37:49 +0600

    .

--- src/components/OnePost/OnePost.jsx
+++ src/components/OnePost/OnePost.jsx
@@ -1,22 +1,27 @@
-import { useNextSanityImage } from 'next-sanity-image'
-import { client } from '../../lib/client'
-import styles from './OnePost.module.scss'
-const OnePost = ({post}) => {
-  const {body, title, mainImage} = post
+import { useNextSanityImage } from "next-sanity-image";
+import Img from "next/image";
+import { client } from "../../lib/client";
+import styles from "./OnePost.module.scss";
+const OnePost = ({ post }) => {
+  const { body, title, mainImage } = post;
 
-  // IMAGE PROPS 
-  const imageProps = useNextSanityImage(
-    client,
-    mainImage.asset._ref
+  // IMAGE PROPS
+  const imageProps = useNextSanityImage(client, mainImage.asset._ref);
+
+  const p = body.map((body) =>
+    body.children.map((child) => child.text).join("")
   );
-
-  const p = body.map(body => body.children.map(child => child.text).join(''))
-  console.log(imageProps)
+  console.log(imageProps);
   return (
     <div className={styles.main}>
       <h1> {title} </h1>
+
+      <div className={styles.imgHold}>
+        <Img {...imageProps} layout="responsive" />
+      </div>
+
       <p> {p} </p>
     </div>
-  )
-}
-export default OnePost
+  );
+};
+export default OnePost;
