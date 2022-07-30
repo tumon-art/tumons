b6d6d02e356479ed20aea8385423c551c01b9962
tumon <tumonkhn@gmail.com>
2022-07-31 05:24:09 +0600

    init

--- src/components/AllPost/AllPost.jsx
+++ src/components/AllPost/AllPost.jsx
@@ -1,17 +1,16 @@
 import Img from "next/image";
 import moment from "moment";
-import Link from 'next/link'
+import Link from "next/link";
 import { useNextSanityImage } from "next-sanity-image";
 import styles from "./AllPost.module.scss";
 import { client } from "../../lib/client";
-
 
 const AllPost = ({ post }) => {
   const { mainImage, title, author, _updatedAt } = post;
   const imageProps = useNextSanityImage(client, mainImage.asset._ref);
   const imageAuthor = useNextSanityImage(client, author.image.asset._ref);
-  const desc = post.body[0].children[0].text.split('.')
-  const time = moment(_updatedAt).format('ll')
+  const desc = post.body[0].children[0].text.split(".");
+  const time = moment(_updatedAt).format("ll");
 
   return (
     <div className={styles.main}>
@@ -20,19 +19,20 @@ const AllPost = ({ post }) => {
       </div>
 
       <Link href={`/post/${post.slug.current}`}>
-        <a>  <h3>{title}</h3> </a>
+        <a>
+          {" "}
+          <h3>{title}</h3>{" "}
+        </a>
       </Link>
 
       <div className={styles.authorInfo}>
-
         <span>
           <div className={styles.imgHoldAuthor}>
-            <Img {...imageAuthor} objectFit='contain' />
+            <Img {...imageAuthor} objectFit="contain" />
           </div>
 
           <p>{author.name}</p>
         </span>
-
 
         {/* === CALANDER AND DATE  */}
         <span>
@@ -52,8 +52,6 @@ const AllPost = ({ post }) => {
 
           <p>{time}</p>
         </span>
-
-
       </div>
 
       <div className={styles.descNbtn}>
@@ -61,15 +59,12 @@ const AllPost = ({ post }) => {
 
         <Link href={`/post/${post.slug.current}`}>
           <a>
-            <button>
-              Show More
-            </button>
+            <button className={styles.btn_20}>Show More</button>
           </a>
         </Link>
       </div>
-
     </div>
   );
 };
 
-export default AllPost
+export default AllPost;
